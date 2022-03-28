1552430bad33e39392431caea8d0e354de80a433
bhavik prajapati <bhavu551999@gmail.com>
2022-03-28 12:52:46 +0530

    today's first commit

--- Helloworld/Methods.cs
+++ Helloworld/Methods.cs
@@ -13,9 +13,17 @@ namespace Helloworld
             Console.WriteLine("----- Learning Functions -----");
             Console.WriteLine("function called : and my is : " + name);
         }
+
+        static int square(int num)
+        {
+            int res = num * num;
+            return res;
+        }
+
         public void learnMethods()
         {
             demo("Bhavik");
+            Console.WriteLine("Square of 4 is : " + square(4));
         }
     }
 }
