047205be25d50c6d15b330cb4382cc366a90e7be
bhavik prajapati <bhavu551999@gmail.com>
2022-03-28 10:57:35 +0530

    today's first commit

--- Helloworld/Arrayss.cs
+++ Helloworld/Arrayss.cs
@@ -11,8 +11,20 @@ namespace Helloworld
         public void learnArray()
         {
             Console.WriteLine("-----Arrays-----");
+
+            /* Creating array with new keyword */
+            int[] ii = new int[2];
+            ii[0] = 1;
+            ii[1] = 2;
+
+            Console.WriteLine(ii[0]);
+
+            /* First way to declare the array with elements */
             string[] arrStr = { "Bhavik", "Jaydeep", "Hemang", "Yash", "Vaibhavi", "Vrutika", "Parth" };
-            Console.WriteLine(arrStr[0]);
+            Console.WriteLine("Array Element : "+arrStr[0]);
+
+            arrStr[1] = "Brp Virus";
+            Console.WriteLine(arrStr[1]);
         }
     }
 }
